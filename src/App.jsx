8a10b86f399717dc6a7dcce3a4ce682025dddb8f
Ilya <ilya@sinyugin.ru>
2022-02-08 23:07:43 +0100

    added connecting to metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb 
+import { useWeb3 } from '@3rdweb/hooks';
+
 const App = () => {
+  //  Use the connectWallet hook thirdweb gives us 
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address: ", address)
+
+  // this is the case where the user hasn't connected their wallet
+  // let them call connectWallet 
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to ESODao</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  // This is the case when we know the user's address
+  // so their wallet is connected to the site. 
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
